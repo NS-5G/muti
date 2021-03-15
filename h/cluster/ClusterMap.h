02184ffc4e7d28c6832b5572505d937c342c5a99
Zhen Xiong <zxiong@panzura.com>
2021-03-15 17:59:28 +0800

    More on ClusterMap.h

--- h/cluster/ClusterMap.h
+++ h/cluster/ClusterMap.h
@@ -10,16 +10,35 @@
 #include <stdbool.h>
 #include <network/Socket.h>
 
-typedef struct Node {
+typedef enum OSDNodeStatus {
+	OSDNodeStatus_Online,
+	OSDNodeStatus_SyncOut,
+	OSDNodeStatus_SyncIn,
+	OSDNodeStatus_Offline
+} OSDNodeStatus;
+
+typedef struct OSDNode {
         uint32_t        id;
+        OSDNodeStatus	status;
         char            host[NETWORK_HOST_LEN + 1];
         int             port;
         void		*user_define;
-} Node;
+} OSDNode;
+
+typedef struct BSet {
+	uint32_t	id;
+	OSDNode		*replicas[];
+} Bset;
+
+typedef enum ClusterMapType {
+	ClusterMapType_MON,
+	ClusterMapType_Client
+} ClusterMapType;
 
 typedef struct ClusterMap ClusterMap;
 typedef struct ClusterMapMethod {
-        void    (*destroy)(ClusterMap*);
+	uint16_t	(*getBSetReplicaLength)(ClusterMap*);
+        void    	(*destroy)(ClusterMap*);
 } ClusterMapMethod;
 
 struct ClusterMap {
@@ -28,8 +47,20 @@ struct ClusterMap {
 };
 
 typedef struct ClusterMapParam {
-
+	ClusterMapType	type;
+	uint16_t	bset_replica_length;
 } ClusterMapParam;
+
+typedef struct ClusterMapMonParam {
+	ClusterMapParam	super;
+	char		*path;
+} ClusterMapMonParam;
+
+typedef struct ClusterMapClientParam {
+	ClusterMapParam	super;
+	char		*mon_host;
+	int		mon_port;
+} ClusterMapClientParam;
 
 bool initClusterMap(ClusterMap*, ClusterMapParam*);
 
