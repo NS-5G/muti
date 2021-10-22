7e4f3bf6d9f160fd2aea8d70c07c2c317dd76a1c
Zhen Xiong <zxiong@panzura.com>
2021-10-22 15:39:06 +0800

    Test timer update

--- cmd/Ut.c
+++ cmd/Ut.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <time.h>
+#include <sys/time.h>
 
 
 int UtThreadPool(int argv, char **argvs);
@@ -49,9 +49,24 @@ TestCase *findTestCase(char *cnm) {
         return NULL;
 }
 
+static inline suseconds_t
+ut_time_diff(struct timeval start, struct timeval end)
+{
+	suseconds_t diff;
+	if (end.tv_sec > start.tv_sec) {
+		diff = (end.tv_sec - start.tv_sec - 1) * 1000000;
+		diff += end.tv_usec + (1000000 - start.tv_usec);
+	} else {
+		//assert(end.tv_sec == start.tv_sec);
+		//assert(end.tv_usec >= start.tv_usec);
+		diff = end.tv_usec - start.tv_usec;
+	}
+	return diff;
+}
+
 int UtDo(int argv, char **argvs) {
 	int ret, i;
-	clock_t start, finish;
+	struct timeval start, end;
 
 	if (argv > 2) {
 		if (strcasecmp(argvs[2], "all") == 0 ) {
@@ -59,20 +74,19 @@ int UtDo(int argv, char **argvs) {
 			while (TestCases[i].name != NULL) {
 
 				printf("\nStart test case %s ...\n", TestCases[i].name);
-				start = clock();
+				gettimeofday(&start, NULL);
 				ret = (*TestCases[i].fn)(0, NULL);
-				finish = clock();
-
-				printf("\nEnd test case %s, Result: %d, Cost time: %ld\n", TestCases[i].name, ret, finish - start);
+				gettimeofday(&end, NULL);
+				printf("\nEnd test case %s, Result: %d, Cost time: %ld\n", TestCases[i].name, ret, ut_time_diff(start, end));
 				i ++;
 			}
 		} else {
 			TestCase *tcase = findTestCase(argvs[2]);
 			if (tcase != NULL) {
-				start = clock();
+				gettimeofday(&start, NULL);
 				ret = (*tcase->fn)(argv, argvs);
-				finish = clock();
-				printf("\nEnd test case %s, Result: %d, Cost time: %ld\n", tcase->name, ret, finish - start);
+				gettimeofday(&end, NULL);
+				printf("\nEnd test case %s, Result: %d, Cost time: %ld\n", tcase->name, ret, ut_time_diff(start, end));
 			} else {
 				goto wrong_test_nm;
 			}
